2fdb4a06c7f18faf4b2e29329a17f0dc06b70675
Zaeem995 <62380620+Zaeem995@users.noreply.github.com>
2023-01-24 14:00:37 +0530

    Update 4_🔥_Heatmap.py

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -23,14 +23,43 @@ st.title("Heatmap")
 
 with st.expander("See source code"):
     with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
-        m.add_heatmap(
-            filepath,
-            latitude="latitude",
-            longitude="longitude",
-            value="pop_max",
-            name="Heat map",
-            radius=20,
-        )
+import streamlit as st
+import leafmap.foliumap as leafmap
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+
+def app():
+
+    st.title('Heatmaps')
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
+
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
+
+    # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
+
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
+    
+    filepath = dataframe 
+    m = leafmap.Map(tiles="stamentoner")
+    m.add_heatmap(
+        filepath,
+        latitude="Lat",
+        longitude="Long",
+        value="Dummy",
+        name="Heat map",
+        radius=20,
+    )
+
 m.to_streamlit(height=700)
